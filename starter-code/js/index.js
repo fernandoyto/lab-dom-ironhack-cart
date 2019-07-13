1bae21781882dbd0f2d0e27c70a339080d2c133a
Fernando Yto <fernando.toshioyto@gmail.com>
2019-07-13 10:28:28 -0300

    keeping track of work done. far from finish line

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,12 +1,12 @@
-function deleteItem(e){
+function deleteItem(e) {
 
 }
 
-function getPriceByProduct(itemNode){
+function getPriceByProduct(itemNode) {
 
 }
 
-function updatePriceByProduct(productPrice, index){
+function updatePriceByProduct(productPrice, index) {
 
 }
 
@@ -14,39 +14,69 @@ function getTotalPrice() {
 
 }
 
-function createQuantityInput(){
+function createQuantityNode() {
 
 }
 
-function createDeleteButton(){
+function createItemNode(dataType, itemData) {
 
 }
 
-function createQuantityNode(){
+function createNewItemRow(itemName, itemUnitPrice) {
+  return `
+  <div class="row">
+    <div>
+      ${itemName}
+    <div>
+    <div>
+      ${itemUnitPrice}
+    </div>
+    <div>
+      <label>QTY</label>
+      <input type "number">
+    </div>
+    <div>
+      PLACEHOLDER
+    </div>
+    <div>
+      <button class="btn btn-delete">Delete</button>
+    </div>
+  </div>
+  `;
+}
+
+function createNewItem() {
 
 }
 
-function createItemNode(dataType, itemData){
-
+function renderForm() {
+  return `<h1>Ironhack Merchandising Shopping</h1>
+  <div class="row">
+    <div>
+      <label>Product Name</label>
+      <input type="text" id="name-input">
+    </div>
+    <div>
+      <label>Product Price</label>
+      <input type="number" id="price-input">
+    </div>
+    <div>
+      <button class="btn btn-success">Create Product</button>
+    </div>
+  </div>
+  `;
 }
 
-function createNewItemRow(itemName, itemUnitPrice){
 
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
-
-  calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
-
-  for(var i = 0; i<deleteButtons.length ; i++){
-    deleteButtons[i].onclick = deleteItem;
-  }
+// eslint-disable-next-line func-names
+window.onload = function () {
+  const body = document.querySelector('body');
+  const html = renderForm();
+  body.innerHTML = html;
+  const createProductButton = document.getElementsByClassName('btn-success');
+  const productList = [];
+  createProductButton[0].onclick = () => {
+    productList.push()
+    createNewItemRow(document.getElementById('name-input').value, document.getElementById('price-input').value);
+  };
 };
